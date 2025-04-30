c984a178b105d86d55fd74e36bfd1d5c2f7c171b
patelabhay672 <patelabhay0302@gmail.com>
2025-04-30 18:36:03 +0530

    server started

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.get('/',(req,res)=>{
-    res.send('Hello World!');
+    res.sendFile(__dirname+'/index.html');
 });
 
 app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
+    console.log('Server is running on port 3000 : http://localhost:3000');
 }   );
