03c73835aa7ff4e5f4989914ad2a450341219354
patelabhay672 <patelabhay0302@gmail.com>
2025-05-01 08:32:51 +0530

    ejs or path.join implement use kiya

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const express= require('express');
 const app= express();
+const path= require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')));
+app.set('view engine','ejs');
 
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname+'/index.html');
+    res.render('index');
 });
 
 app.listen(3000,()=>{
