8eca2d5f2437d4aacd7fee2ebe90e207382568e3
patelabhay672 <patelabhay0302@gmail.com>
2025-05-01 16:22:01 +0530

    complete the task manager

--- script.js
+++ script.js
@@ -1,25 +1,77 @@
-const express= require('express');
-const app= express();
-const path= require('path');
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require("fs");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname,'public')));
-app.set('view engine','ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
 
-app.get('/',(req,res)=>{
-    res.render('index');
+app.get("/", (req, res) => {
+  fs.readdir(`./files`, function (err, files) {
+    res.render("index", { files: files });
+  });
 });
 
-app.get("/profile/:username",function(req,res){
-    res.send(`Hello ${req.params.username}`);
+app.get("/file/:filename", (req, res) => {
+  fs.readFile(`./files/${req.params.filename}`, "utf8", function (err, data) {
+    res.render("show", { file: req.params.filename, data: data });
+  });
 });
 
-app.get("/auther/:username/:age",function(req,res){
-    res.send(`Hello ${req.params.username} and your age is ${req.params.age}`);
+app.get("/all-tasks", (req, res) => {
+  fs.readdir("./files", (err, files) => {
+    if (err) {
+      console.error(err);
+      res.send("Error reading files");
+    } else {
+      res.render("tasklist", { files });
+    }
+  });
 });
 
+app.post("/delete-task/:filename", (req, res) => {
+  fs.unlink(`./files/${req.params.filename}`, function (err) {
+    if (err) {
+      console.log(err);
+      res.send("Error deleting file");
+    } else {
+      res.redirect("/");
+    }
+  });
+});
 
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000 : http://localhost:3000');
-}   );
+app.post("/update-task/:filename", (req, res) => {
+  fs.writeFile(
+    `./files/${req.params.filename}`,
+    req.body.updatedData,
+    function (err) {
+      if (err) {
+        console.log(err);
+        res.send("Error updating file");
+      } else {
+        res.redirect(`/file/${req.params.filename}`);
+      }
+    }
+  );
+});
+
+app.post("/create", (req, res) => {
+  fs.writeFile(
+    `./files/${req.body.title.split(" ").join("_")}.txt`,
+    req.body.details,
+    function (err) {
+      if (err) {
+        console.log(err);
+        res.send("Error creating file");
+      } else {
+        res.redirect("/");
+      }
+    }
+  );
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000 : http://localhost:3000");
+});
